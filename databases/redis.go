ce6a493529edabda6eea1bf8397de920f134b913
kiana kholousi <kianakholousi@gmail.com>
2023-06-09 21:26:04 +0330

    Update redis.go
    +add connection to Redis
    -should fix config package issue

--- databases/redis.go
+++ databases/redis.go
@@ -1,1 +1,26 @@
 package databases
+
+import (
+	"fmt"
+
+	config "aliagha/config"
+
+	"github.com/go-redis/redis"
+)
+
+func init_redis() {
+	var configObj *config.Configs
+	// Connect to Redis
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", configObj.DBConfig.Host, configObj.DBConfig.Port),
+		Password: configObj.DBConfig.Password,
+		DB:       0, // use default database
+	})
+
+	// Test the connection
+	pong, err := client.Ping().Result()
+	if err != nil {
+		panic(fmt.Errorf("Error connecting to Redis: %s", err))
+	}
+	fmt.Println("connected to Redis database: ", pong)
+}
