b246a6bfb0107e2b519c2556d8d67d40753c1f30
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-13 23:18:41 +0330

    move migrate function from mysql.go to migrate.go

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -1,16 +1,17 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
 	"aliagha/config"
+	"database/sql"
 	"errors"
 	"fmt"
 	"strconv"
 
-	db "aliagha/database"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
 
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/spf13/cobra"
 )
 
@@ -33,7 +34,7 @@ Flags:
 	-a, --action string   Action to perform: "up" or "down" (required)
 	-c, --config string   Path to custom configuration file in YAML format (required)`,
 	Run: func(cmd *cobra.Command, args []string) {
-		migrate()
+		migrateCobra()
 	},
 }
 
@@ -66,18 +67,62 @@ func init() {
 	migrateCmd.Flags().StringVarP(&Config, "config", "c", "", "path to custom configuration file in YAML format")
 }
 
-func migrate() {
+func migrateCobra() {
 	cfg, err := config.Init(config.Params{FilePath: Config, FileType: "yaml"})
 	if err != nil {
 		panic(err)
 	}
-	username := cfg.Database.Username
-	password := cfg.Database.Password
-	host := cfg.Database.Host
-	port := cfg.Database.Port
-	dbname := cfg.Database.Name
-	address := cfg.Database.MigrationAddress
 
-	fmt.Println(db.MigrateDB(username, password, host, strconv.Itoa(port), dbname, address, Action.String()))
+	err = MigrateDB(
+		cfg.Database.Username,
+		cfg.Database.Password,
+		cfg.Database.Host,
+		strconv.Itoa(cfg.Database.Port),
+		cfg.Database.Name,
+		cfg.Database.MigrationAddress,
+		Action.String())
+	if err != nil && err.Error() != "no change" {
+		panic(err)
+	}
 
+}
+
+func MigrateDB(username, password, host, port, dbname, address, action string) error {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/",
+		username, password, host, port)
+
+	dbStart, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+	_, err = dbStart.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", "aliagha"))
+	if err != nil {
+		panic(err)
+	}
+
+	dataSourceName = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?multiStatements=true",
+		username, password, host, port, dbname)
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		return err
+	}
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+	if err != nil {
+		return err
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		fmt.Sprintf("file://%s", address),
+		"mysql",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	if action == "up" {
+		err = m.Up()
+	} else {
+		err = m.Down()
+	}
+	return err
 }
--- database/mysql.go
+++ database/mysql.go
@@ -1,64 +1,17 @@
 package database
 
 import (
-	"database/sql"
 	"fmt"
 
-	"github.com/golang-migrate/migrate/v4"
-	msql "gorm.io/driver/mysql"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-
-	_ "github.com/go-sql-driver/mysql"
-
-	"github.com/golang-migrate/migrate/v4/database/mysql"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
-
-func MigrateDB(username, password, host, port, dbname, address, action string) error {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/",
-		username, password, host, port)
-
-	dbStart, err := sql.Open("mysql", dataSourceName)
-	if err != nil {
-		panic(err)
-	}
-	_, err = dbStart.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", "aliagha"))
-	if err != nil {
-		panic(err)
-	}
-
-	dataSourceName = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?multiStatements=true",
-		username, password, host, port, dbname)
-	db, err := sql.Open("mysql", dataSourceName)
-	if err != nil {
-		return err
-	}
-	driver, err := mysql.WithInstance(db, &mysql.Config{})
-	if err != nil {
-		return err
-	}
-	m, err := migrate.NewWithDatabaseInstance(
-		fmt.Sprintf("file://%s", address),
-		"mysql",
-		driver,
-	)
-	if err != nil {
-		return err
-	}
-
-	if action == "up" {
-		err = m.Up()
-	} else {
-		err = m.Down()
-	}
-	return err
-}
 
 func InitDB(username, password, host, name, dbname string, port int) (*gorm.DB, error) {
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
 		username, password, host, port, name)
-	db, err := gorm.Open(msql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
