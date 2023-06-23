bfb22da6d58d15e4b9ed3e5c2881ff79dd5073d1
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-23 12:49:13 +0330

    make email unique in user model

--- models/user.go
+++ models/user.go
@@ -7,7 +7,7 @@ type User struct {
 	Name      string    `gorm:"column:name;not null" json:"name"`
 	Password  string    `gorm:"column:password;not null" json:"password"`
 	Cellphone string    `gorm:"column:cellphone;not null" json:"cellphone"`
-	Email     string    `gorm:"column:email;not null" json:"email"`
+	Email     string    `gorm:"column:email;not null;uniqueIndex" json:"email"`
 	CreatedAt time.Time `gorm:"column:created_at;default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt time.Time `gorm:"column:updated_at;default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
