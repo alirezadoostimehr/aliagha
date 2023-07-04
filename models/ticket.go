bb648448b72bbbc36e7b4a1255fcfca0e349444d
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-07-05 01:22:05 +0330

    remove passenger foreign key from ticket model

--- models/ticket.go
+++ models/ticket.go
@@ -7,7 +7,6 @@ type Ticket struct {
 	UID       int32     `gorm:"column:u_id;not null" json:"u_id"`
 	User      User      `gorm:"foreignKey:UID"`
 	PIDs      string    `gorm:"column:p_ids;not null" json:"p_ids"`
-	Passenger Passenger `gorm:"foreignKey:PID"`
 	FID       int32     `gorm:"column:f_id;not null" json:"f_id"`
 	Flight    Flight    `gorm:"foreignKey:FID"`
 	Status    string    `gorm:"column:status;not null" json:"status"`
