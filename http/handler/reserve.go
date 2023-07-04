0042d129323d700a8911fd790f227c94c8e83727
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-07-05 00:50:55 +0330

    add checker for passenger being for user

--- http/handler/reserve.go
+++ http/handler/reserve.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"aliagha/models"
 	"aliagha/services"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
@@ -15,7 +16,7 @@ type FlightReservation struct {
 }
 
 type FlightReservationRequest struct {
-	UserId       int   `json:"-"`
+	UserId       int   `json:"user_id"` // Should be taken from context
 	FlightId     int   `json:"flight_id" validate:"required"`
 	PassengerIds []int `json:"passenger_ids" validate:"required"`
 }
@@ -28,6 +29,25 @@ func (f *FlightReservation) Reserve(ctx echo.Context) error {
 
 	if err := f.Validator.Struct(&req); err != nil {
 		return ctx.JSON(http.StatusBadRequest, "Bad Request")
+	}
+
+	//req.UserId = ctx.Get("user_id").(int)
+	for _, passengerId := range req.PassengerIds {
+		var exists bool
+		err := f.DB.
+			Debug().
+			Model(&models.Passenger{}).
+			Select("count(*) > 0").
+			Where("id = ? AND u_id = ?", passengerId, req.UserId).
+			First(&exists).
+			Error
+		if err != nil {
+			return ctx.JSON(http.StatusInternalServerError, "Internal Server Error")
+		}
+
+		if exists == false {
+			return ctx.JSON(http.StatusBadRequest, "Passengers Not Allowed")
+		}
 	}
 
 	return ctx.JSON(http.StatusOK, req)
